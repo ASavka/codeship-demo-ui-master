66945a046ddb0f6db4b705de9a176d19560c4a50
anton <antsavka@lohika.com>
2018-03-09 18:13:46 +0200

    first tony's commit

---
+++ src/main/java/com/example/kostiademoui/KostiaDemoUiApplication.java
@@ -0,0 +1,12 @@
+package com.example.kostiademoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostiaDemoUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostiaDemoUiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/kostiademoui/Test1.java
@@ -0,0 +1,15 @@
+package com.example.kostiademoui;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void method1() {
+        System.out.println("test - method 1");
+    }
+
+    @Test
+    public void method2() {
+        System.out.println("test - method 2");
+    }
+}
